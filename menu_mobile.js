597d2b0a186c61388817464ca5b78c2a87c503ef
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-03-02 11:56:48 -0300

    atualizacao

--- menu_mobile.js
+++ menu_mobile.js
@@ -1,3 +1,7 @@
 const botao__mobile = document.querySelector('.botao__mobile');
-console.log(botao__mobile)
+const menu__navegacao = document.querySelector('menu__navegacao')
+
+botao__mobile.addEventListener('click', () => {
+    menu.classList.toggle('active')
+}) 
 
